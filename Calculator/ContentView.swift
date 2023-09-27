4e2994e5664f25c8dac0fc4ef12e5694f03204d1
rongyu1121 <940899489@qq.com>
2023-09-27 22:17:05 +0800

    第二章

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CalculatorButton(title: "1", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground") {
+                print("Button: 1")
+            }
+            CalculatorButton(title: "2", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground") {
+                print("Button: 2")
+            }
+            CalculatorButton(title: "3", size: CGSize(width: 88, height: 88), backgroundColorName: "digitBackground") {
+                print("Button: 3")
+            }
+            CalculatorButton(title: "+", size: CGSize(width: 88, height: 88), backgroundColorName: "operatorBackground") {
+                print("Button: +")
+            }
         }
-        .padding()
+    }
+}
+
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    var body: some View {
+        Button(action: action, label: {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundStyle(.white)
+                .frame(width: size.width, height: size.height)
+                .background(Color(backgroundColorName))
+                .clipShape(Circle())
+        })
     }
 }
 
